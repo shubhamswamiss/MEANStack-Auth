96ddce738d6a24b3736906d95af3ef8302124746
shubham.swami <shubham.swami@epps-erp.in>
2024-04-10 19:46:51 +0530

    post request

--- api/index.js
+++ api/index.js
@@ -1,10 +1,37 @@
-import express from  'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import roleRoute from './routes/role.js';
 const app = express();
+dotenv.config(); 
 
-app.use('/',(req,res)=>{
-  return res.send("Hello, Welcome to MEAN Stack Project");
-})
+app.use(express.json());
+app.use('/api/role',roleRoute);
+// app.use('/',(req,res)=>{
+//   return res.send("<h1>Hello, Welcome to MEAN Stack Project</h1>");
+// })
 
-app.listen(8080,()=>{
-    console.log('Connected to backend!');
-})
+// app.use('/api/login',(req,res)=>{
+//     return res.send('<h1>Login is Success!!</h1>');
+// })
+
+// app.use('/api/register',(req,res)=>{
+//     return res.send('<h1>Hello, Welcome to MEAN Stack Project</h1>');
+// })
+
+// DB Connection
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to Database!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+
+app.listen(8080, () => {
+  connectMongoDB();
+  console.log("Connected to backend!");
+});
---
+++ api/models/User.js
@@ -0,0 +1,47 @@
+import mongoose,{Schema} from 'mongoose';
+
+const UserSchema = mongoose.Schema(
+    {
+        firstName:{
+            type: String,
+            required: true
+        },
+        lastName:{
+            type: String,
+            required:true
+        },
+        username: {
+            type: String,
+            required:true,
+            unique:true 
+        },
+        email: {
+            type: String,
+            required:true,
+            unique:true 
+        },
+        password: {
+            type: String,
+            required:true
+        },
+        profileImage: {
+            type: String,
+            required: false,
+            default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Duser&psig=AOvVaw0vz4c6UL_EoaOkIz8eXa_c&ust=1712839748296000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLivr7jXt4UDFQAAAAAdAAAAABAE'
+        },
+        isAdmin: {
+            type: Boolean,
+            default: true
+        },
+        roles: {
+            type: [Schema.Types.ObjectId],
+            required: true,
+            ref:'Role'
+        }
+    },
+    {
+        timestamps:true
+    }
+);
+
+export default mongoose.model('User', UserSchema);
