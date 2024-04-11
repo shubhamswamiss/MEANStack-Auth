82f6bf11ff2167691d4fe0b3385973aa77167382
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-04-11 17:06:05 +0530

    test

--- api/routes/role.js
+++ api/routes/role.js
@@ -3,13 +3,13 @@ import { createRole, getAllRoles, updateRole } from '../controllers/role.control
 
 const router = express.Router();
 
-//Create a new role in DB.
+//Create a new role in DB
 router.post('/create', createRole);
 
-//Update role in DB.
+//Update role in DB
 router.put('/update/:id', updateRole);
 
-//Get all the roles from DB.
+//Get all the roles from DB
 router.get('/getAll', getAllRoles);
 
 export default router;
