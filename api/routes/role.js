496ecf585a3bb37be48d07753ecb4c9046a2775a
shubhamswamiss <shubhamswamiss333@gamil.com>
2024-04-11 16:37:00 +0530

    create controller

--- api/routes/role.js
+++ api/routes/role.js
@@ -1,21 +1,15 @@
 import express from 'express';
-import Role from  '../models/Role.js';
+import { createRole, getAllRoles, updateRole } from '../controllers/role.controller.js';
 
 const router = express.Router();
 
 //Create a new role in DB
-router.post('/create', async (req,res,next)=>{
-   try{
-      if(req.body.role && req.body.role != ''){
-         const newRole = new Role(req.body);
-         await newRole.save();
-         return res.send("Role Created!");
-      }else{
-        return res.status(400).send('Bad Request');
-      }
-   }catch(error){
-       res.status(500).send("Internal Server Error!");  
-   }
-});
+router.post('/create', createRole);
+
+//Update role in DB
+router.put('/update/:id', updateRole);
+
+//Get all the roles from DB
+router.get('/getAll', getAllRoles);
 
 export default router;
