e75d3746527e058acd4cf91d45c2c7834ce8e195
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-04-13 22:25:48 +0530

    adding logic to controller

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,8 +1,10 @@
-import { CreateError } from "../utils/error"
+import { CreateError } from "../utils/error.js"
+import { CreateSuccess } from "../utils/success.js";
 
 export const getAllUsers = async (req,res,next)=>{
     try {
-        
+        const users = await User.find({});
+        return next(CreateSuccess(200,"All Users",users));
     } catch (error) {
        return next(CreateError(500,'Internal Server Error!')); 
     }
@@ -10,7 +12,10 @@ export const getAllUsers = async (req,res,next)=>{
 
 export const getById = async (req,res,next)=>{
      try {
-        
+        const user = await User.findById(req.params.id);
+        if(!user)
+             return next(CreateError(404,"User Not Found!"));
+         return next(CreateSuccess(200,"Single User",user));
      } catch (error) {
         return next(CreateError(500,'Internal Server Error!'));   
      }
