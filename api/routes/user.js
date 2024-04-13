ac13bdcd90ada9cedbaaf4a41f424bd39126b347
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-04-13 22:24:13 +0530

    verify routes using token

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getAllUsers, getById } from '../controllers/user.controller.js';
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 //Get all users
-router.get('/',getAllUsers);
+router.get('/', verifyAdmin, getAllUsers);
 
 //Get by id
-router.get('/:id',getById)
+router.get('/:id', verifyUser, getById)
 
 export default router;
