7791540bb8657b05e1f477b7da65d413e8f8dd73
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-04-16 18:04:58 +0530

    API Intigration

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { confirmPasswordValidator } from 'src/app/validators/confirm-password.validator';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -11,13 +13,16 @@ import { confirmPasswordValidator } from 'src/app/validators/confirm-password.va
 })
 export default class RegisterComponent implements OnInit {
   fb = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
   registerForm !:FormGroup;
+
   ngOnInit():void{
      this.registerForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['',Validators.compose([Validators.required, Validators.email])],
-      username: ['',Validators.required],
+      userName: ['',Validators.required],
       password: ['',Validators.required],
       confirmPassword: ['',Validators.required]
      },
@@ -27,6 +32,16 @@ export default class RegisterComponent implements OnInit {
   }
 
   register(){
-    console.log(this.registerForm.value);
+     this.authService.registerService(this.registerForm.value).subscribe({
+      next: (res) => {
+        alert("User Created!");
+        console.log(res);
+        this.registerForm.reset();
+        this.router.navigate(['login']);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+     })
   }
 }
