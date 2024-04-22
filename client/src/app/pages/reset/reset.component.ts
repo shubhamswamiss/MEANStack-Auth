e05ad990692d655c455ae5519af9ed2d6cc2e0e9
shubhamswamiss <shubhamswamiss333@gmail.com>
2024-04-22 19:49:34 +0530

    reset password page

--- client/src/app/pages/reset/reset.component.ts
+++ client/src/app/pages/reset/reset.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { confirmPasswordValidator } from 'src/app/validators/confirm-password.validator';
 
 @Component({
   selector: 'app-reset',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reset.component.html',
   styleUrls: ['./reset.component.css']
 })
-export default class ResetComponent {
+export default class ResetComponent implements OnInit {
+  resetForm!: FormGroup;
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
+  token!: string;
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((val) => {
+      this.token = val['token'];
+    })
+    this.resetForm = this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    },
+      {
+        validator: confirmPasswordValidator('password', 'confirmPassword')
+      });
+
+  }
+
+  reset() {
+
+  }
 }
